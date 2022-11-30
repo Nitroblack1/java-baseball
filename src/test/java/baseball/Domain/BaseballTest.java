70a6121d072ed3c995c063d48b908c3e8b475f93
DESKTOP-MG9CFDI\PC <nitroblue@naver.com>
2022-12-01 01:04:46 +0900

    feat(BaseballTest) : implement test for game start functionality

--- src/test/java/baseball/Domain/BaseballTest.java
+++ src/test/java/baseball/Domain/BaseballTest.java
@@ -1,5 +1,20 @@
 package baseball.Domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BaseballTest {
 
+    @DisplayName("게임 시작시 정답 추출하여 Referee에 전달 테스트")
+    @Test
+    void gameStartTest() {
+        Baseball game = new Baseball();
+
+        assertThat(game.referee.getAnswer().size()).isEqualTo(3);
+    }
 }
